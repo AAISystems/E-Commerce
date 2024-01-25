771dcfde0cd6174c68bccefdca350d9533e33404
Alvaro Martinez Lineros <alvaro.martinez.lineros@gmail.com>
2024-01-25 10:34:09 +0100

    Implementación bbdd

--- E-commerce/database/migrations/2014_10_12_000000_create_users_table.php
+++ E-commerce/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,11 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->date("birth_date");
+            $table->integer('telefono')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedBigInteger('roles_id');
             $table->rememberToken();
             $table->timestamps();
         });

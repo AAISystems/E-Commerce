8d816217abe35dd047786fa0dc95a0dea95d445f
Alvaro Martinez Lineros <alvaro.martinez.lineros@gmail.com>
2024-02-19 09:23:15 +0100

    Iconos y favicon

--- E-commerce/resources/views/template/template.blade.php
+++ E-commerce/resources/views/template/template.blade.php
@@ -6,13 +6,13 @@
     <!-- Required meta tags -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-
-    <script defer src="{{asset('js/cartQuantity.js')}}"></script>
+    <link rel="shortcut icon" href="{{ asset('img/lavandaResize.png') }}" type="image/png">
+    <script defer src="{{ asset('js/cartQuantity.js') }}"></script>
     @yield('script')
     <!-- Bootstrap CSS v5.2.1 -->
     @vite(['resources/js/app.js', 'resources/css/app.scss', 'resources/css/app.css'])
-    
-    
+
+
 
 </head>
 
